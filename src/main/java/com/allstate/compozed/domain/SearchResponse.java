7ed95ae2b10e4c60e6933fca6be32601dc73a3c9
adkins2010 <michael.michinobu.adkins@gmail.com>
2017-05-02 19:19:18 -0400

    Add files via upload

---
+++ src/main/java/com/allstate/compozed/domain/SearchResponse.java
@@ -0,0 +1,69 @@
+package com.allstate.compozed.domain;
+
+import com.fasterxml.jackson.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by localadmin on 3/17/17.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "Search",
+        "totalResults",
+        "Response"
+})
+public class SearchResponse {
+
+    @JsonProperty("Search")
+    private List<Search> search = null;
+    @JsonProperty("totalResults")
+    private String totalResults;
+    @JsonProperty("Response")
+    private String response;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("Search")
+    public List<Search> getSearch() {
+        return search;
+    }
+
+    @JsonProperty("Search")
+    public void setSearch(List<Search> search) {
+        this.search = search;
+    }
+
+    @JsonProperty("totalResults")
+    public String getTotalResults() {
+        return totalResults;
+    }
+
+    @JsonProperty("totalResults")
+    public void setTotalResults(String totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    @JsonProperty("Response")
+    public String getResponse() {
+        return response;
+    }
+
+    @JsonProperty("Response")
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
